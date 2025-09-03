18f4b4cf60b94d19bea306abba83ea30e8268168
farkas.norbert <farkas.norbert@ckik.hu>
2025-09-03 22:53:48 +0200

    ismétlés, 1-triangle

---
+++ 1-ismetles/1-triangle/Triangle/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Triangle
+{
+    internal class Program
+    {
+        static void F1(Random r)
+        {
+            Console.WriteLine("1. feladat:");
+            int a = r.Next(1, 21);
+            int b = r.Next(1, 21);
+            int c = r.Next(1, 21);
+            Console.WriteLine($"Oldalak: {a} {b} {c}");
+            if (a + b > c && b + c > a && c + a > b)
+            {
+                Console.WriteLine("Szerkeszthető!");
+            }
+            else
+            {
+                Console.WriteLine("Nem szerkeszthető!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            F1(r);
+            F2(r);
+        }
+
+        private static void F2(Random r)
+        {
+            Console.WriteLine("2. feladat:");
+            int count = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                int a = r.Next(1, 21);
+                int b = r.Next(1, 21);
+                int c = r.Next(1, 21);
+                Console.WriteLine($"Oldalak: {a} {b} {c}");
+                if (a*a + b*b == c*c)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Derékszögű háromszögek száma: {count}");
+        }
+    }
+}
