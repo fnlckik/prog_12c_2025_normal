a28138b2bee4e4ab896983ef5199ca3f7351111c
farkas.norbert <farkas.norbert@ckik.hu>
2025-09-05 11:20:30 +0200

    Roulette - Plain(), GetBet()

--- 1-ismetles/2-roulette/Roulette/Program.cs
+++ 1-ismetles/2-roulette/Roulette/Program.cs
@@ -1,16 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Roulette
 {
     internal class Program
     {
+        static void Plain(int number, int bet, ref int money)
+        {
+            Random r = new Random();
+            int winner = 5;//r.Next(37);
+            Console.WriteLine($"Nyertes szám: {winner}");
+            if (number == winner)
+            {
+                Console.WriteLine($"Nyeremény: {bet * 36}");
+                money += bet * 36;
+            }
+            else
+            {
+                Console.WriteLine("Nem nyert!");
+            }
+        }
+
+        static void GetBet(out int bet)
+        {
+            Console.Write("Tét: ");
+            bet = int.Parse(Console.ReadLine());
+        }
+
         static void Main(string[] args)
         {
-
+            int money = 500;
+            while (money > 0)
+            {
+                Console.Write("Szám: ");
+                int number = int.Parse(Console.ReadLine());
+                int bet;
+                GetBet(out bet);
+                money -= bet;
+                Plain(number, bet, ref money);
+                Console.WriteLine($"Aktuális egyenleg: {money}\n");
+            }
         }
     }
 }
