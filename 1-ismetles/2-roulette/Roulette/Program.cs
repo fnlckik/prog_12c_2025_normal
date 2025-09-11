273d343e139709ebf5ba1e3e45c801e3f55dd8fb
farkas.norbert <farkas.norbert@ckik.hu>
2025-09-11 10:40:48 +0200

    Rulett játékmódok - szám / szín / vége

--- 1-ismetles/2-roulette/Roulette/Program.cs
+++ 1-ismetles/2-roulette/Roulette/Program.cs
@@ -14,20 +14,40 @@ namespace Roulette
         static public HashSet<int> red = new HashSet<int>();
         static public HashSet<int> black = new HashSet<int>();
 
+        static char GetColor(int n)
+        {
+            if (red.Contains(n)) return 'R';
+            else if (black.Contains(n)) return 'B';
+            else return 'G';
+        }
+
         static void Plain(int number, int bet, ref int money)
         {
             //Thread.Sleep(1);
             //Random r = new Random();
             int winner = r.Next(37);
-            char color;
-            if (red.Contains(winner)) color = 'R';
-            else if (black.Contains(winner)) color = 'B';
-            else color = 'G';
+            char color = GetColor(winner);
             Console.WriteLine($"Nyertes szám: {winner} ({color})");
             if (number == winner)
             {
                 Console.WriteLine($"Nyeremény: {bet * 36}");
                 money += bet * 36;
+            }
+            else
+            {
+                Console.WriteLine("Nem nyert!");
+            }
+        }
+
+        static void RedBlack(string choice, int bet, ref int money)
+        {
+            int winner = r.Next(37);
+            char color = GetColor(winner);
+            Console.WriteLine($"Nyertes szám: {winner} ({color})");
+            if (choice == color.ToString())
+            {
+                Console.WriteLine($"Nyeremény: {bet * 2}");
+                money += bet * 2;
             }
             else
             {
@@ -42,15 +62,6 @@ namespace Roulette
                 Console.Write("Tét: ");
                 bet = int.Parse(Console.ReadLine());
             } while (bet > money || bet <= 0);
-        }
-
-        static void Main(string[] args)
-        {
-            int money = 500;
-            Console.WriteLine($"Kezdőpénz: {money}");
-            ReadFromFile("szinek.txt");
-            //Play(money);
-            AutoPlay(money);
         }
 
         // Beolvas egy sort, és a set halmazba eltárolja.
@@ -89,11 +100,18 @@ namespace Roulette
             bool isFinished = false;
             while (money > 0 && !isFinished)
             {
-                Console.Write("Folytassuk? (Szám vagy N): ");
-                string choice = Console.ReadLine();
+                Console.Write("Folytassuk? Szám (0-36) | Red (R) | Black (B) | No (N)): ");
+                string choice = Console.ReadLine().ToUpper();
                 if (choice == "N")
                 {
                     isFinished = true;
+                }
+                else if (choice == "R" || choice == "B")
+                {
+                    GetBet(out int bet, money);
+                    money -= bet;
+                    RedBlack(choice, bet, ref money);
+                    Console.WriteLine($"Aktuális egyenleg: {money}\n");
                 }
                 else
                 {
@@ -104,6 +122,15 @@ namespace Roulette
                     Console.WriteLine($"Aktuális egyenleg: {money}\n");
                 }
             }
+        }
+
+        static void Main(string[] args)
+        {
+            int money = 500;
+            Console.WriteLine($"Kezdőpénz: {money}");
+            ReadFromFile("szinek.txt");
+            Play(money);
+            //AutoPlay(money);
         }
     }
 }
