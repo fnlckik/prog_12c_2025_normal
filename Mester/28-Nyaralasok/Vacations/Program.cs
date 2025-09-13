01c47e013eef706324da33fa69ebb90c99c045cb
farkas.norbert <farkas.norbert@ckik.hu>
2025-09-13 13:11:31 +0200

    Vacations - ReadFromFile, Task1

--- Mester/28-Nyaralasok/Vacations/Program.cs
+++ Mester/28-Nyaralasok/Vacations/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Vacations
 {
     internal class Program
     {
+        struct Vacation
+        {
+            public int studentId;
+            public string place;
+        }
+
         static void Main(string[] args)
         {
+            List<Vacation> vacations = new List<Vacation>();
+            ReadFromFile(vacations, out int studentCount, "be1.txt");
+            //Console.WriteLine(vacations.Count);
+            T1(vacations);
+        }
+
+        private static void T1(List<Vacation> vacations)
+        {
+            Console.WriteLine("#");
+            HashSet<string> places = new HashSet<string>();
+            foreach (Vacation vacation in vacations)
+            {
+                places.Add(vacation.place);
+            }
+            Console.WriteLine(places.Count);
+        }
+
+        private static void ReadFromFile(List<Vacation> vacations, out int studentCount, string file)
+        {
+            StreamReader sr = new StreamReader(file);
+            string[] temp = sr.ReadLine().Split();
+            studentCount = int.Parse(temp[0]);
+            while (!sr.EndOfStream)
+            {
+                Vacation vacation;
+                vacation.studentId = int.Parse(sr.ReadLine());
+                vacation.place = sr.ReadLine();
+                vacations.Add(vacation);
+            }
+            sr.Close();
         }
     }
 }
