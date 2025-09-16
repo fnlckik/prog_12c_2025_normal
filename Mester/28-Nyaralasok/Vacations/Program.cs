e75e56f30afa2059e04e21ac4eef3e9a0811568e
farkas.norbert <farkas.norbert@ckik.hu>
2025-09-16 16:15:22 +0200

    Nyaralások - Task2, Task3 (dictionary)

--- Mester/28-Nyaralasok/Vacations/Program.cs
+++ Mester/28-Nyaralasok/Vacations/Program.cs
@@ -18,6 +18,53 @@ namespace Vacations
             ReadFromFile(vacations, out int studentCount, "be1.txt");
             //Console.WriteLine(vacations.Count);
             T1(vacations);
+            T2(vacations);
+            T3(vacations);
+        }
+
+        static void T3(List<Vacation> vacations)
+        {
+            Console.WriteLine("#");
+            Dictionary<string, int> placeCounts = new Dictionary<string, int>();
+            foreach (Vacation vacation in vacations)
+            {
+                if (placeCounts.ContainsKey(vacation.place))
+                {
+                    placeCounts[vacation.place]++;
+                }
+                else
+                {
+                    placeCounts.Add(vacation.place, 1);
+                }
+            }
+            string maxPlace = "";
+            int maxValue = -1;
+            // Maximum kiválasztás tétel!
+            foreach (KeyValuePair<string, int> item in placeCounts)
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+            }
+        }
+
+        private static void T2(List<Vacation> vacations)
+        {
+            Console.WriteLine("#");
+            Dictionary<int, int> studentCounts = new Dictionary<int, int>();
+            foreach (Vacation vacation in vacations)
+            {
+                if (studentCounts.ContainsKey(vacation.studentId))
+                {
+                    studentCounts[vacation.studentId]++;
+                }
+                else
+                {
+                    studentCounts.Add(vacation.studentId, 1);
+                }
+            }
+            foreach (KeyValuePair<int, int> item in studentCounts)
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+            }
         }
 
         private static void T1(List<Vacation> vacations)
