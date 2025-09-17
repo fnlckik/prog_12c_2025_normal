4daa913e5ffea5a7f89218130bef2d70eee3c09b
farkas.norbert <farkas.norbert@ckik.hu>
2025-09-17 18:04:14 +0200

    Nyaralások - Task4 (nem nyaraló diák)

--- Mester/28-Nyaralasok/Vacations/Program.cs
+++ Mester/28-Nyaralasok/Vacations/Program.cs
@@ -18,8 +18,28 @@ namespace Vacations
             ReadFromFile(vacations, out int studentCount, "be1.txt");
             //Console.WriteLine(vacations.Count);
             T1(vacations);
-            T2(vacations);
+            Dictionary<int, int> studentCounts = T2(vacations);
             T3(vacations);
+            //T4(studentCount, studentCounts);
+            T4(studentCount, vacations);
+        }
+
+        static void T4(int studentCount, List<Vacation> vacations)
+        {
+            List<bool> hasVacation = new List<bool>();
+            // TODO
+        }
+
+        static void T4(int studentCount, Dictionary<int, int> studentCounts)
+        {
+            Console.WriteLine("#");
+            int i = 1;
+            while (i <= studentCount && studentCounts.ContainsKey(i))
+            {
+                i++;
+            }
+            if (i <= studentCount) Console.WriteLine(i);
+            else Console.WriteLine(-1);
         }
 
         static void T3(List<Vacation> vacations)
@@ -40,13 +60,27 @@ namespace Vacations
             string maxPlace = "";
             int maxValue = -1;
             // Maximum kiválasztás tétel!
-            foreach (KeyValuePair<string, int> item in placeCounts)
+            // Homoki Márk módszer:
+            //foreach (KeyValuePair<string, int> item in placeCounts)
+            //{
+            //    if (item.Value > maxValue)
+            //    {
+            //        maxValue = item.Value;
+            //        maxPlace = item.Key;
+            //    }
+            //}
+            foreach (string place in placeCounts.Keys)
             {
-                Console.WriteLine($"{item.Key} {item.Value}");
+                if (placeCounts[place] > maxValue)
+                {
+                    maxValue = placeCounts[place];
+                    maxPlace = place;
+                }
             }
+            Console.WriteLine(maxPlace);
         }
 
-        private static void T2(List<Vacation> vacations)
+        private static Dictionary<int, int> T2(List<Vacation> vacations)
         {
             Console.WriteLine("#");
             Dictionary<int, int> studentCounts = new Dictionary<int, int>();
@@ -65,6 +99,7 @@ namespace Vacations
             {
                 Console.WriteLine($"{item.Key} {item.Value}");
             }
+            return studentCounts;
         }
 
         private static void T1(List<Vacation> vacations)
