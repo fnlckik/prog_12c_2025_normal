29fe7816177c57d5d494e078056024dba242fdaf
farkas.norbert <farkas.norbert@ckik.hu>
2025-09-21 14:28:07 +0200

    mester - uniquebirds mátrix ismétlés

--- Mester/2-UniqueBirds/UniqueBirds/Program.cs
+++ Mester/2-UniqueBirds/UniqueBirds/Program.cs
@@ -9,11 +9,69 @@ namespace UniqueBirds
         static void Main(string[] args)
         {
             List<List<int>> amounts = new List<List<int>>();
-            ReadFromFile(amounts, "be1.txt");
+            ReadFromFile(amounts, "be2.txt");
             //ShowMatrix(amounts);
-            Console.WriteLine(IsUnique(amounts, 0)); // false
-            Console.WriteLine(IsUnique(amounts, 1)); // true
-            Console.WriteLine(IsUnique(amounts, 2)); // true
+            //Console.WriteLine(IsUnique(amounts, 0)); // false
+            //Console.WriteLine(IsUnique(amounts, 1)); // true
+            //Console.WriteLine(IsUnique(amounts, 2)); // true
+
+            // ~TDD: Test Driven Development
+            //Console.WriteLine(HasUniqueBird(amounts, 0)); // false
+            //Console.WriteLine(HasUniqueBird(amounts, 1)); // true
+            //Console.WriteLine(HasUniqueBird(amounts, 2)); // true
+            //Console.WriteLine(HasUniqueBird(amounts, 3)); // false
+
+            List<int> places = GetPlaces(amounts); // 2 3
+            ShowPlaces(places);
+        }
+
+        static void ShowPlaces(List<int> places)
+        {
+            Console.WriteLine(places.Count);
+            foreach (int place in places)
+            {
+                Console.Write($"{place + 1} ");
+            }
+        }
+
+        // Melyik helyeken van egyedi madár? (kiválogatás)
+        static List<int> GetPlaces(List<List<int>> amounts)
+        {
+            List<int> places = new List<int>();
+            for (int i = 0; i < amounts.Count; i++)
+            {
+                if (HasUniqueBird(amounts, i))
+                {
+                    places.Add(i);
+                }
+            }
+            return places;
+        }
+
+        // Van-e egyedi madár a kapott helységben? (eldöntés)
+        static bool HasUniqueBird(List<List<int>> amounts, int placeIndex)
+        {
+            int j = 0; // i. madarat vizsgáljuk
+            while (j < amounts[placeIndex].Count && !(IsUnique(amounts, j) && amounts[placeIndex][j] > 0))
+            {
+                j++;
+            }
+            return j < amounts[placeIndex].Count;
+        }
+
+        // Egyedi-e a kapott madár? (megszámolás)
+        static bool IsUnique(List<List<int>> amounts, int birdIndex)
+        {
+            // Hány nullától különböző érték van az oszlopban?
+            int count = 0;
+            for (int i = 0; i < amounts.Count; i++) // i. sor
+            {
+                if (amounts[i][birdIndex] != 0)
+                {
+                    count++;
+                }
+            }
+            return count == 1;
         }
 
         static void ShowMatrix(List<List<int>> amounts)
