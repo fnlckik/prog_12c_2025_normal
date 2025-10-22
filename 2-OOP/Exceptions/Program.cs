cad483f8dc28366c2d8609534c7ad62e2c66b19f
farkas.norbert <farkas.norbert@ckik.hu>
2025-10-22 11:52:36 +0200

    Exceptions - fájlok kezelése

--- 2-OOP/Exceptions/Program.cs
+++ 2-OOP/Exceptions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Exceptions
 {
@@ -40,10 +41,73 @@ namespace Exceptions
             Console.WriteLine();
         }
 
+        static void E3()
+        {
+            Console.WriteLine("3. példa: tömb indexelés");
+            string[] gyumolcsok = { "alma", "banán", "citrom", "dinnye" };
+            Console.Write("Elem sorszáma: ");
+            try
+            {
+                int i = int.Parse(Console.ReadLine());
+                Console.WriteLine($"A {i}. elem: {gyumolcsok[i-1]}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Adj meg egy egész számot!");
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"Adj meg egy 1 és {gyumolcsok.Length} közötti számot!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine();
+        }
+
+        static void E4()
+        {
+            Console.WriteLine("4. példa: fájlok");
+            Console.Write("Fájl neve: ");
+            string fileName = Console.ReadLine();
+            try
+            {
+                StreamReader sr = new StreamReader(fileName);
+                string[] temp = sr.ReadLine().Split(' ');
+                int a = int.Parse(temp[0]);
+                int b = int.Parse(temp[1]);
+                Console.WriteLine($"Beolvasott számok: {a} és {b}.");
+                sr.Close();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("A fájl első sorában szóközzel elválasztva legyen legalább két egész szám!");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("A fájlban csak egész számok legyenek!");
+            }
+            //catch (Exception e) // üres fájl (NullReferenceException)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+            // Van itt egy nagy gond még...
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            E1();
-            E2();
+            //E1();
+            //E2();
+            //E3();
+            E4();
             Console.WriteLine("Program vége.");
         }
     }
