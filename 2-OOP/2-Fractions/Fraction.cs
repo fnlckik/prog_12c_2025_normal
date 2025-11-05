30a8bdf4b2ab5da09f101b7daf188ee227257034
farkas.norbert <farkas.norbert@ckik.hu>
2025-11-05 10:02:28 +0100

    Fraction - fields, constructor

---
+++ 2-OOP/2-Fractions/Fraction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2_Fractions
+{
+    internal class Fraction
+    {
+        // numerator, denominator: a / b
+        private int a;
+        private int b;
+
+        public Fraction(int a, int b)
+        {
+            if (b == 0)
+            {
+                string message = $"Hiba: {a} / {b}. Nem lehet 0 a nevező.";
+                throw new ArgumentException(message);
+            }
+            this.a = a;
+            this.b = b;
+        }
+    }
+}
---
+++ 2-OOP/2-Fractions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2_Fractions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Fraction a = new Fraction(3, 4);
+                Fraction hiba = new Fraction(2, 0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
