65e423db44c152c2025a061a8eccc8a35677ccb9
farkas.norbert <farkas.norbert@ckik.hu>
2025-11-06 08:46:11 +0100

    Fraction - operator ==

--- 2-OOP/2-Fractions/Fraction.cs
+++ 2-OOP/2-Fractions/Fraction.cs
@@ -13,10 +13,37 @@ namespace _2_Fractions
             if (b == 0)
             {
                 string message = $"Hiba: {a} / {b}. Nem lehet 0 a nevező.";
-                throw new ArgumentException(message);
+                throw new DivideByZeroException(message);
             }
             this.a = a;
             this.b = b;
+        }
+
+        public double Value => (double)this.a / this.b;
+
+        public override string ToString()
+        {
+            return $"{this.a} / {this.b}";
+        }
+
+        public static bool operator ==(Fraction x, Fraction y)
+        {
+            return x.a == y.a && x.b == y.b;
+        }
+
+        public static bool operator !=(Fraction x, Fraction y)
+        {
+            return !(x == y);
+        }
+
+        // Kéne: legnagyobb közös osztó
+        // Euklideszi algoritmus
+        public void Simplify()
+        {
+            // a = 12, b = 15, r = 12
+            // a = 15, b = 12, r = 3
+            // a = 12, b = 3, r = 0
+            // Amikor r == 0, a b-ben van a legnagyobb közös osztó.
         }
     }
 }
--- 2-OOP/2-Fractions/Program.cs
+++ 2-OOP/2-Fractions/Program.cs
@@ -9,12 +9,22 @@ namespace _2_Fractions
             try
             {
                 Fraction a = new Fraction(3, 4);
+                Console.WriteLine($"Az első tört: {a}, értéke: {a.Value}");
                 Fraction hiba = new Fraction(2, 0);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+            Console.WriteLine();
+
+            // Operátorok
+            Fraction x = new Fraction(4, 5);
+            Fraction y = new Fraction(4, 5);
+            Console.WriteLine($"{x} == {y}: {x == y}");
+            //Console.WriteLine(0.1 + 0.2 == 0.3);
+            Fraction z = new Fraction(12, 15);
+            Console.WriteLine($"{x} == {z}: {x == z}");
         }
     }
 }
