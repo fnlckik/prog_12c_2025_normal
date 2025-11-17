5fa4a5c8f62676e5c1bd195154af0703a0881fd4
farkas.norbert <farkas.norbert@ckik.hu>
2025-11-17 22:11:11 +0100

    Öröklődés - elmélet

--- 2-OOP/4-Animals/Dog.cs
+++ 2-OOP/4-Animals/Dog.cs
@@ -4,6 +4,16 @@ namespace Animals
 {
     internal class Dog : Animal
     {
+        // Inheritance: öröklődés
+        // Dog: leszármazott (derived class)
+        // Animal: ősosztály (base class)
+
+        // Az öröklődés olyan OOP technika,
+        // amely során a leszármazott osztály örökli 
+        // az ősosztály adattagjait, metódusait.
+
+        // A leszármazott osztálynak kötelező meghívni
+        // az ősosztály konstruktorát.
         public string breed; // fajta
 
         public Dog(string name, int age, string breed) : base(name, age)
