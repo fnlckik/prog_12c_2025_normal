61836a167322e64a04a8406d1ad228389f81fc43
farkas.norbert <farkas.norbert@ckik.hu>
2025-11-26 09:02:29 +0100

    Varosok feladatok

---
+++ Dolgozat/2-Varosok/Cities/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cities
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3. feladat:");
+            //City cegled = new City("Cegléd", "Magyarország", 36125, 1.5);
+            //Console.WriteLine(cegled);
+
+            Console.WriteLine("\n4. feladat:");
+            //Console.WriteLine($"Cegléd 1 főre jutó GPD-je: {cegled.GDPPerCapita:0.} USD");
+
+            Console.WriteLine("\n9. feladat:");
+            //CityCollection collection = new CityCollection("cities.txt");
+            //City poorest = collection.PoorestCity();
+            //Console.WriteLine($"A legkisebb GPT/fő-vel rendelkező város: {poorest}");
+
+            // ------------------ 11. feladat ------------------
+            Console.WriteLine("\n11. feladat:");
+            
+            // -------------------------------------------------
+        }
+    }
+}
