06fe7c82517632ccf75c02657a43e4e4aab79c2e
farkas.norbert <farkas.norbert@ckik.hu>
2025-12-03 17:01:37 +0100

    Vizeskancso + táblázatok

--- IKT/vizeskancso/Forras/script.js
+++ IKT/vizeskancso/Forras/script.js
@@ -57,5 +57,46 @@ function pour(e) {
     otherFilled += amount;
     fillRows(currentTable.rows, currentFilled);
     fillRows(otherTable.rows, otherFilled);
+    if (currentFilled === 6 || otherFilled === 6) {
+        const winDiv = document.querySelector("#win");
+        winDiv.style.display = "block";
+    }
 }
 div.addEventListener("click", pour);
+
+function clear(e) {
+    const button = e.target;
+    if (button.id.split("-")[1] !== "clear") return;
+    const div = button.parentNode;
+    const currentTable = div.querySelector("table");
+    let currentFilled = currentTable.querySelectorAll(".filled").length;
+    currentFilled = 0;
+    fillRows(currentTable.rows, currentFilled);
+}
+div.addEventListener("click", clear);
+
+const table = document.querySelector("#test");
+function showCell(e) {
+    const td = e.target;
+    const j = td.cellIndex;
+    const tr = td.parentNode;
+    const i = tr.rowIndex;
+    console.log(`(${i}, ${j})`);
+}
+table.addEventListener("click", showCell);
+
+// Random szám generálás [a..b]-on.
+function randint(a, b) {
+    return Math.floor(Math.random() * (b-a+1)) + a;
+}
+
+function randomize(e) {
+    if (e.code !== "KeyR") return;
+    const n = table.rows.length;
+    const m = table.rows[0].cells.length;
+    const i = randint(0, n-1);
+    const j = randint(0, m-1);
+    const td = table.rows[i].cells[j];
+    td.innerText = "alma";
+}
+window.addEventListener("keyup", randomize);
