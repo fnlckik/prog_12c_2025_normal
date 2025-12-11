4f92c0358841effceb3ffe7014d1c756d6e0d735
farkas.norbert <farkas.norbert@ckik.hu>
2025-12-11 17:25:22 +0100

    LINQ Basics - Where, Select, Count, Sum, Min, Max, Any, All, First, Last

--- 3-LINQ/1-Basics/Program.cs
+++ 3-LINQ/1-Basics/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Basics
@@ -85,13 +86,51 @@ namespace Basics
             Write("Pozitív elemek (halmaz):", F(set, IsPositive));
             Write("Pozitív elemek (tömb):", F(array, IsPositive));
             Write("Pozitív elemek (tömb):", array.Where(IsPositive));
-
+            Write("Pozitív elemek (tömb):", array.Where(x => x > 0)); // lambda expression
+            
             // Páros elemek - kiválogatás
             Console.WriteLine();
             Write("Páros elemek (lista):", F(list, IsEven));
             Write("Páros elemek (halmaz):", F(set, IsEven));
             Write("Páros elemek (tömb):", F(array, IsEven));
             Write("Páros elemek (tömb):", array.Where(IsEven));
+            Write("Páros elemek (tömb):", array.Where(x => x % 2 == 0));
+
+            // LINQ = Language INtegrated Query
+            // sablon problémák => progtételek
+            // Imperatív programozás: Hogyan akarjuk megoldani? (if, for, ...) -> C
+
+            // progtételek => segédfüggvények
+            // Deklaratív programozás: Mit akarunk megoldani? (SELECT, WHERE, ...) -> SQL
+
+            // Progtételek
+            // 1. Kiválogatás: Where
+            Console.WriteLine();
+            Write("1. A 10 és 20 közötti elemek:", array.Where(x => x >= 10 && x <= 20));
+
+            // 2. Másolás: Select
+            Write("2. Elemek négyzetei:", array.Select(x => x * x));
+
+            // 3. Megszámolás: Count
+            Console.WriteLine("3. Pozitív elemek száma: " + array.Count(x => x > 0));
+
+            // 4. Összegzés: Sum
+            Console.WriteLine("4. Elemek összege: " + array.Sum());
+            Console.WriteLine("4. Elemek átlaga: " + array.Average());
+
+            // 5. Minmax: Min, Max
+            Console.WriteLine("5. Legnagyobb elem: " + array.Max());
+
+            // 6. Eldöntés: Any
+            Console.WriteLine("6. Van-e 10-nél nagyobb szám? " + array.Any(x => x > 10));
+
+            // 7. Eldöntés (optimista): All
+            Console.WriteLine("7. Minden elem 10-nél nagyobb? " + array.All(x => x > 10));
+
+            // 8. Keresés: First, Last
+            //List<int> empty = new List<int>();
+            Console.WriteLine("8. Az első páros elem: " + array.First(x => x % 2 == 0));
+            Console.WriteLine("8. Az utolsó páros elem: " + array.Last(x => x % 2 == 0));
         }
     }
 }
