146fcb015cdf57a92a1485d6efb60bfbb8c25cf4
farkas.norbert <farkas.norbert@ckik.hu>
2026-01-13 21:16:22 +0100

    LINQ query syntax

--- 3-LINQ/5-BookShop/Program.cs
+++ 3-LINQ/5-BookShop/Program.cs
@@ -35,8 +35,47 @@ namespace BookShop
             Print("Szerzők:", authors);
             Print("\nKönyvek:", books);
             Print("\nRendelések:", orders);
-            //Console.Clear();
+            Console.Clear();
             #endregion
+
+            // 1. A szerzők nevei
+            // m = method syntax, q = query syntax
+            var m1 = authors.Select(a => a.Name);
+            Print("1. A szerzők nevei: ", m1);
+
+            // Emlék: SQL
+            // SELECT Name
+            // FROM authors;
+
+            var q1 = from a in authors
+                     select a.Name;
+            Print("1. A szerzők nevei: ", q1);
+
+            // 2. A 400 oldalnál hosszabb könyvek: cím, oldalszám
+            var m2 = books.Where(b => b.Pages > 400).Select(b => new { b.Title, b.Pages });
+            Print("2. A 400 oldalnál hosszabb könyvek: ", m2);
+
+            var q2 = from b in books
+                     where b.Pages > 400
+                     select new { b.Title, b.Pages };
+            Print("2. A 400 oldalnál hosszabb könyvek: ", q2);
+
+            // 3. Fantasy könyvek, év szerint növekvően
+            var m3 = books.Where(b => b.Genre == "Fantasy").OrderBy(b => b.Year).ThenBy(b => b.Pages);
+            Print("3. Fantasy könyvek, év szerint növekvően: ", m3);
+
+            // Query szintaxis "mindig" select-re végződik!
+            var q3 = from b in books
+                     where b.Genre == "Fantasy"
+                     orderby b.Year, b.Pages
+                     select b;
+            Print("3. Fantasy könyvek, év szerint növekvően: ", q3);
+
+            // 4. A 6 legdrágább könyv
+            var q4 = (from b in books
+                     orderby b.Price descending
+                     select b).Take(6);
+            Print("4. A 6 legdrágább könyv: ", q4);
         }
     }
 }
