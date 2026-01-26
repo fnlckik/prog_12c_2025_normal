4e749af16e49743f174c43ede3e66c7a42f3fb52
farkas.norbert <farkas.norbert@ckik.hu>
2026-01-26 19:17:31 +0100

    enum, DialogResult, TextBox, Label, CheckBox

--- 4-Windows-Forms/1-Hello/Form1.Designer.cs
+++ 4-Windows-Forms/1-Hello/Form1.Designer.cs
@@ -29,25 +29,76 @@
         private void InitializeComponent()
         {
             this.HelloBtn = new System.Windows.Forms.Button();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.GreetLabel = new System.Windows.Forms.Label();
+            this.BoldCheckBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // HelloBtn
             // 
+            this.HelloBtn.Enabled = false;
             this.HelloBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.HelloBtn.Location = new System.Drawing.Point(266, 168);
+            this.HelloBtn.Location = new System.Drawing.Point(484, 86);
             this.HelloBtn.Name = "HelloBtn";
-            this.HelloBtn.Size = new System.Drawing.Size(231, 103);
+            this.HelloBtn.Size = new System.Drawing.Size(231, 87);
             this.HelloBtn.TabIndex = 0;
             this.HelloBtn.Text = "Hello!";
             this.HelloBtn.UseVisualStyleBackColor = true;
             this.HelloBtn.Click += new System.EventHandler(this.HelloBtn_Click);
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.NameTextBox.Location = new System.Drawing.Point(200, 115);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(184, 29);
+            this.NameTextBox.TabIndex = 1;
+            this.NameTextBox.TextChanged += new System.EventHandler(this.NameTextBox_TextChanged);
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.NameLabel.Location = new System.Drawing.Point(125, 115);
+            this.NameLabel.Margin = new System.Windows.Forms.Padding(3, 0, 10, 0);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(62, 29);
+            this.NameLabel.TabIndex = 2;
+            this.NameLabel.Text = "Név:";
+            // 
+            // GreetLabel
+            // 
+            this.GreetLabel.AutoSize = true;
+            this.GreetLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.GreetLabel.Location = new System.Drawing.Point(479, 209);
+            this.GreetLabel.Name = "GreetLabel";
+            this.GreetLabel.Size = new System.Drawing.Size(0, 29);
+            this.GreetLabel.TabIndex = 3;
+            // 
+            // BoldCheckBox
+            // 
+            this.BoldCheckBox.AutoSize = true;
+            this.BoldCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BoldCheckBox.Location = new System.Drawing.Point(130, 183);
+            this.BoldCheckBox.Name = "BoldCheckBox";
+            this.BoldCheckBox.Size = new System.Drawing.Size(138, 33);
+            this.BoldCheckBox.TabIndex = 4;
+            this.BoldCheckBox.Text = "Félkövér?";
+            this.BoldCheckBox.UseVisualStyleBackColor = true;
+            this.BoldCheckBox.CheckedChanged += new System.EventHandler(this.BoldCheckBox_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 438);
+            this.Controls.Add(this.BoldCheckBox);
+            this.Controls.Add(this.GreetLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.NameTextBox);
             this.Controls.Add(this.HelloBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MinimizeBox = false;
@@ -55,12 +106,17 @@
             this.ShowIcon = false;
             this.Text = "Hello Grafikus Világ!";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button HelloBtn;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label GreetLabel;
+        private System.Windows.Forms.CheckBox BoldCheckBox;
     }
 }
 
--- 4-Windows-Forms/1-Hello/Form1.cs
+++ 4-Windows-Forms/1-Hello/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace _1_Hello
 {
@@ -15,6 +16,7 @@ namespace _1_Hello
         // enum => felsorolás
         private enum Hetvege
         { 
+            Pentek = 5,
             Szombat = 6,
             Vasarnap = 7
         };
@@ -26,10 +28,71 @@ namespace _1_Hello
 
         private void HelloBtn_Click(object sender, EventArgs e)
         {
+            // 1-Hello -> Properties -> Output type
             //Console.WriteLine("Hello Világ!");
-            //DialogResult result = MessageBox.Show("Hello Világ!", "Felhasználó köszöntése", MessageBoxButtons.YesNoCancel);
-            //MessageBox.Show(result == ??);
-            MessageBox.Show(((int)Hetvege.Szombat).ToString());
+            // (MessageBoxButtons)3 == MessageBoxButtons.YesNoCancel
+            string name = NameTextBox.Text;
+            if (name != "")
+            {
+                GreetLabel.Text = $"Hello {name}!";
+            }
+            /*
+            DialogResult result = MessageBox.Show($"Hello {name}!",
+                                                  "Felhasználó köszöntése",
+                                                  MessageBoxButtons.YesNoCancel,
+                                                  MessageBoxIcon.Information);
+            */
+            //if (result == DialogResult.Yes)
+            //{
+            //    MessageBox.Show("Az IGEN gombra nyomtál!");
+            //}
+            //else if (result == DialogResult.No)
+            //{
+            //    MessageBox.Show("A NEM gombra nyomtál!");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("A MÉGSE gombra nyomtál!");
+            //}
+            /*
+            switch (result)
+            {
+                case DialogResult.Cancel:
+                    MessageBox.Show("A MÉGSE gombra nyomtál!");
+                    break;
+                case DialogResult.Yes:
+                    MessageBox.Show("Az IGEN gombra nyomtál!");
+                    break;
+                case DialogResult.No:
+                    MessageBox.Show("A NEM gombra nyomtál!");
+                    break;
+            }
+            */
+            //MessageBox.Show($"{Hetvege.Szombat}");
+        }
+
+        private void NameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //GreetLabel.Text = $"Hello {NameTextBox.Text}!";
+            //if (NameTextBox.Text != "")
+            //{
+            //    HelloBtn.Enabled = true;
+            //}
+            //else
+            //{
+            //    HelloBtn.Enabled = false;
+            //}
+            HelloBtn.Enabled = NameTextBox.Text != "";
+        }
+
+        private void BoldCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(BoldCheckBox.Checked.ToString());
+            if (BoldCheckBox.Checked)
+            {
+                // ???
+                //GreetLabel.Font.Bold = true;
+            }
         }
     }
 }
