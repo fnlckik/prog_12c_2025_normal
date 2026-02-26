ceb025d87908d04f06a9954f2d892a4c2221261a
farkas.norbert <farkas.norbert@ckik.hu>
2026-02-26 14:37:54 +0100

    Adatok generálása, megjelenítése

--- 4-Windows-Forms/5-GlovesFactory/DataForm.Designer.cs
+++ 4-Windows-Forms/5-GlovesFactory/DataForm.Designer.cs
@@ -28,28 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.StatsGroupBox = new System.Windows.Forms.GroupBox();
             this.MagnitudeLabel = new System.Windows.Forms.Label();
             this.MedianLabel = new System.Windows.Forms.Label();
             this.SpreadLabel = new System.Windows.Forms.Label();
             this.AverageLabel = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ExtremeGroupBox = new System.Windows.Forms.GroupBox();
             this.ExtremeCheckBox = new System.Windows.Forms.CheckBox();
             this.TopNumUpDown = new System.Windows.Forms.NumericUpDown();
             this.BottomNumUpDown = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.MaterialGridView = new System.Windows.Forms.DataGridView();
+            this.MaterialDataGrid = new System.Windows.Forms.DataGridView();
             this.menuStrip = new System.Windows.Forms.MenuStrip();
             this.adatokToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.RandomMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.StatsGroupBox.SuspendLayout();
+            this.ExtremeGroupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.TopNumUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BottomNumUpDown)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.MaterialGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaterialDataGrid)).BeginInit();
             this.menuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -63,20 +64,20 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Kategória:";
             // 
-            // groupBox1
+            // StatsGroupBox
             // 
-            this.groupBox1.Controls.Add(this.MagnitudeLabel);
-            this.groupBox1.Controls.Add(this.MedianLabel);
-            this.groupBox1.Controls.Add(this.SpreadLabel);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.AverageLabel);
-            this.groupBox1.Enabled = false;
-            this.groupBox1.Location = new System.Drawing.Point(29, 247);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(378, 174);
-            this.groupBox1.TabIndex = 4;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Statisztikák";
+            this.StatsGroupBox.Controls.Add(this.MagnitudeLabel);
+            this.StatsGroupBox.Controls.Add(this.MedianLabel);
+            this.StatsGroupBox.Controls.Add(this.SpreadLabel);
+            this.StatsGroupBox.Controls.Add(this.label1);
+            this.StatsGroupBox.Controls.Add(this.AverageLabel);
+            this.StatsGroupBox.Enabled = false;
+            this.StatsGroupBox.Location = new System.Drawing.Point(29, 247);
+            this.StatsGroupBox.Name = "StatsGroupBox";
+            this.StatsGroupBox.Size = new System.Drawing.Size(378, 174);
+            this.StatsGroupBox.TabIndex = 4;
+            this.StatsGroupBox.TabStop = false;
+            this.StatsGroupBox.Text = "Statisztikák";
             // 
             // MagnitudeLabel
             // 
@@ -114,20 +115,20 @@
             this.AverageLabel.TabIndex = 0;
             this.AverageLabel.Text = "Átlag:";
             // 
-            // groupBox2
+            // ExtremeGroupBox
             // 
-            this.groupBox2.Controls.Add(this.ExtremeCheckBox);
-            this.groupBox2.Controls.Add(this.TopNumUpDown);
-            this.groupBox2.Controls.Add(this.BottomNumUpDown);
-            this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Enabled = false;
-            this.groupBox2.Location = new System.Drawing.Point(29, 50);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(160, 178);
-            this.groupBox2.TabIndex = 5;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Kiugró adatok";
+            this.ExtremeGroupBox.Controls.Add(this.ExtremeCheckBox);
+            this.ExtremeGroupBox.Controls.Add(this.TopNumUpDown);
+            this.ExtremeGroupBox.Controls.Add(this.BottomNumUpDown);
+            this.ExtremeGroupBox.Controls.Add(this.label3);
+            this.ExtremeGroupBox.Controls.Add(this.label2);
+            this.ExtremeGroupBox.Enabled = false;
+            this.ExtremeGroupBox.Location = new System.Drawing.Point(29, 50);
+            this.ExtremeGroupBox.Name = "ExtremeGroupBox";
+            this.ExtremeGroupBox.Size = new System.Drawing.Size(160, 178);
+            this.ExtremeGroupBox.TabIndex = 5;
+            this.ExtremeGroupBox.TabStop = false;
+            this.ExtremeGroupBox.Text = "Kiugró adatok";
             // 
             // ExtremeCheckBox
             // 
@@ -179,13 +180,29 @@
             this.panel1.Size = new System.Drawing.Size(200, 178);
             this.panel1.TabIndex = 5;
             // 
-            // MaterialGridView
+            // MaterialDataGrid
             // 
-            this.MaterialGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.MaterialGridView.Location = new System.Drawing.Point(425, 37);
-            this.MaterialGridView.Name = "MaterialGridView";
-            this.MaterialGridView.Size = new System.Drawing.Size(472, 384);
-            this.MaterialGridView.TabIndex = 6;
+            this.MaterialDataGrid.AllowUserToAddRows = false;
+            this.MaterialDataGrid.AllowUserToDeleteRows = false;
+            this.MaterialDataGrid.AllowUserToResizeColumns = false;
+            this.MaterialDataGrid.AllowUserToResizeRows = false;
+            this.MaterialDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.MaterialDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MaterialDataGrid.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.MaterialDataGrid.DefaultCellStyle = dataGridViewCellStyle1;
+            this.MaterialDataGrid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.MaterialDataGrid.Location = new System.Drawing.Point(425, 37);
+            this.MaterialDataGrid.Name = "MaterialDataGrid";
+            this.MaterialDataGrid.RowHeadersVisible = false;
+            this.MaterialDataGrid.Size = new System.Drawing.Size(472, 384);
+            this.MaterialDataGrid.TabIndex = 6;
             // 
             // menuStrip
             // 
@@ -193,7 +210,7 @@
             this.adatokToolStripMenuItem});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(909, 36);
+            this.menuStrip.Size = new System.Drawing.Size(909, 38);
             this.menuStrip.TabIndex = 7;
             this.menuStrip.Text = "menuStrip1";
             // 
@@ -202,13 +219,14 @@
             this.adatokToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.RandomMenuItem});
             this.adatokToolStripMenuItem.Name = "adatokToolStripMenuItem";
-            this.adatokToolStripMenuItem.Size = new System.Drawing.Size(88, 32);
+            this.adatokToolStripMenuItem.Size = new System.Drawing.Size(91, 34);
             this.adatokToolStripMenuItem.Text = "Adatok";
             // 
             // RandomMenuItem
             // 
             this.RandomMenuItem.Name = "RandomMenuItem";
-            this.RandomMenuItem.Size = new System.Drawing.Size(180, 32);
+            this.RandomMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
+            this.RandomMenuItem.Size = new System.Drawing.Size(236, 34);
             this.RandomMenuItem.Text = "Random";
             this.RandomMenuItem.Click += new System.EventHandler(this.RandomMenuItem_Click);
             // 
@@ -217,10 +235,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(909, 435);
-            this.Controls.Add(this.MaterialGridView);
-            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.MaterialDataGrid);
+            this.Controls.Add(this.ExtremeGroupBox);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.StatsGroupBox);
             this.Controls.Add(this.menuStrip);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -229,13 +247,13 @@
             this.MaximizeBox = false;
             this.Name = "DataForm";
             this.Text = "Kesztyűgyár - Felhasznált bőr";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
+            this.StatsGroupBox.ResumeLayout(false);
+            this.StatsGroupBox.PerformLayout();
+            this.ExtremeGroupBox.ResumeLayout(false);
+            this.ExtremeGroupBox.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.TopNumUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.BottomNumUpDown)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.MaterialGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaterialDataGrid)).EndInit();
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
@@ -245,19 +263,19 @@
 
         #endregion
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox StatsGroupBox;
         private System.Windows.Forms.Label SpreadLabel;
         private System.Windows.Forms.Label AverageLabel;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label MagnitudeLabel;
         private System.Windows.Forms.Label MedianLabel;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox ExtremeGroupBox;
         private System.Windows.Forms.CheckBox ExtremeCheckBox;
         private System.Windows.Forms.NumericUpDown TopNumUpDown;
         private System.Windows.Forms.NumericUpDown BottomNumUpDown;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView MaterialGridView;
+        private System.Windows.Forms.DataGridView MaterialDataGrid;
         private System.Windows.Forms.MenuStrip menuStrip;
         private System.Windows.Forms.ToolStripMenuItem adatokToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem RandomMenuItem;
--- 4-Windows-Forms/5-GlovesFactory/DataForm.cs
+++ 4-Windows-Forms/5-GlovesFactory/DataForm.cs
@@ -20,7 +20,54 @@ namespace GlovesFactory
             data = new List<int>();
             Random r = new Random();
             // 123 adat
+            for (int i = 0; i < 123; i++)
+            {
+                double p = r.NextDouble(); // [0, 1)
+                int n;
+                if (p < 0.25)
+                {
+                    n = r.Next(50, 60);
+                }
+                else if (p < 0.95)
+                {
+                    n = r.Next(60, 70);
+                }
+                else
+                {
+                    n = r.Next(90, 100);
+                }
+                data.Add(n);
+            }
+            MaterialDataGrid.ColumnCount = 8;
+            MaterialDataGrid.RowCount = (int)Math.Ceiling((double)data.Count / 8);
+            int rowIndex = 0;
+            int colIndex = 0;
+            foreach (int number in data)
+            {
+                MaterialDataGrid.Rows[rowIndex].Cells[colIndex].Value = number;
+                MaterialDataGrid.Rows[rowIndex].Height = MaterialDataGrid.Height / 8;
+                colIndex++;
+                if (colIndex > 7)
+                {
+                    rowIndex++;
+                    colIndex = 0;
+                }
+            }
+            //MaterialDataGrid.Rows[0].Cells[0].Value = data[0];
+            //MaterialDataGrid.Rows[0].Cells[1].Value = data[1];
+            //MaterialDataGrid.Rows[0].Cells[2].Value = data[2];
+            //MaterialDataGrid.Rows[0].Cells[3].Value = data[3];
 
+            //MaterialDataGrid.Rows[1].Cells[0].Value = data[8];
+            //MaterialDataGrid.Rows[1].Cells[1].Value = data[9];
+            //MaterialDataGrid.Rows[1].Cells[2].Value = data[10];
+            //for (int i = 0; i < data.Count; i++)
+            //{
+            //    MaterialDataGrid.Rows[i / 8].Cells[i % 8].Value = data[i];
+            //    MaterialDataGrid.Rows[i / 8].Height = MaterialDataGrid.Height / 8;
+            //}
+            StatsGroupBox.Enabled = true;
+            ExtremeGroupBox.Enabled = true;
         }
     }
 }
