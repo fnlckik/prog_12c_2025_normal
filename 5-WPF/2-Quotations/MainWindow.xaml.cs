f3587e198c5619cb6b9a3509fdc62796dedc9b91
farkas.norbert <farkas.norbert@ckik.hu>
2026-04-20 00:35:21 +0200

    WPF Binding

--- 5-WPF/2-Quotations/MainWindow.xaml.cs
+++ 5-WPF/2-Quotations/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Security.Policy;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit.Core;
 
 namespace _2_Quotations
 {
@@ -16,9 +21,45 @@ namespace _2_Quotations
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Csak public property-t lehet Binding-nál használni.
+        public ObservableCollection<Quotation> Quotes { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Quotes = [];
+            DataContext = this;
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new();
+            dialog.Filter = "Szöveges fájl|*.txt";
+            //dialog.InitialDirectory = AppContext.BaseDirectory;
+            bool? result = dialog.ShowDialog();
+            if (result != true) return;
+            string path = dialog.FileName;
+            using StreamReader sr = new(path); // le is zárja automatikusan
+            Quotes.Clear();
+            while (!sr.EndOfStream)
+            {
+                // null forgiving operator !.
+                // "Biztosan tudom, hogy nem null."
+                string[] temp = sr.ReadLine()!.Split("|");
+                string author = temp[0];
+                string title = temp[1];
+                int year = int.Parse(temp[2]);
+                string text = temp[3];
+                Quotation q = new(author, title, year, text);
+                Quotes.Add(q);
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            QuotationTextBlock.Visibility = Visibility.Visible;
+            Quotation q = (Quotation)QuotationComboBox.SelectedItem;
+            QuotationTextBlock.Text = q.Text;
         }
     }
 }
---
+++ 5-WPF/2-Quotations/Quotation.cs
@@ -0,0 +1,23 @@
+﻿namespace _2_Quotations
+{
+    public class Quotation
+    {
+        public Quotation(string author, string title, int year, string text)
+        {
+            Author = author;
+            Title = title;
+            Year = year;
+            Text = text;
+        }
+
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return string.Join("", Text.Take(20)) + "...";
+        }
+    }
+}
