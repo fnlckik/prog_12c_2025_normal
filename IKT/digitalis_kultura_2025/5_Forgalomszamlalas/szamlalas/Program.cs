59095837053e49e01fe876834b935d76a19364da
farkas.norbert <farkas.norbert@ckik.hu>
2026-05-18 12:03:43 +0200

    szamlalas megoldás

--- IKT/digitalis_kultura_2025/5_Forgalomszamlalas/szamlalas/Program.cs
+++ IKT/digitalis_kultura_2025/5_Forgalomszamlalas/szamlalas/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// F1
+List<int> numbers = [];
+string path = "../../../../meres.txt";
+using StreamReader sr = new(path);
+// x => int.Parse(x)
+numbers = sr.ReadLine()!.Split(", ").Select(int.Parse).ToList();
+//Console.WriteLine(numbers.Count);
+
+// F2
+Console.WriteLine("2. feladat");
+int totalCount = numbers.Where(x => x != -1).Sum();
+Console.WriteLine($"Összesen {totalCount} kerékpárost számoltak.");
+
+// F3
+Console.WriteLine("\n3. feladat");
+Console.WriteLine("Óránkénti mérések:");
+int hour = 6;
+int sum = 0; // Összegezzük az aktuális óra adatait
+for (int i = 0; i < numbers.Count; i++)
+{
+	if (numbers[i] != -1) sum += numbers[i];
+	if (i % 4 == 3)
+	{
+        Console.WriteLine($"{hour} órától {sum} kerékpáros");
+		hour++;
+		sum = 0;
+	}
+}
+
+// F4
+Console.WriteLine("\n4. feladat");
+int maxValue = numbers.Max();
+
+int maxIndex = numbers.IndexOf(maxValue);
+int start = 360; // 6:00
+int minute = start + (maxIndex + 1) * 15;
+int maxHour = minute / 60;
+int maxMinute = minute % 60;
+
+Console.WriteLine($"Az áthaladók maximális száma: {maxValue}; a rögzítés időpontja: {maxHour}:{maxMinute}."); // 8:30
---
+++ IKT/digitalis_kultura_2025/5_Forgalomszamlalas/szamlalas/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamlalas
+{
+    internal class Test
+    {
+        private List<int> szamok;
+        private int Elemszam => szamok.Count();
+    }
+}
